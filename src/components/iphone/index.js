68bd79f267b629855bc18bcfb9ed473c5ca3eaaa
Milena <94915179+milenaserych@users.noreply.github.com>
2023-03-15 11:28:27 +0000

    Adding Weather API key

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -23,7 +23,8 @@ export default class Iphone extends Component {
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=cf17e23b1d108b29a4d738d2084baf5";
+		// my API key: 1812aa1047a527c9537d5e2315c80ba0
+		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=1812aa1047a527c9537d5e2315c80ba0";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
