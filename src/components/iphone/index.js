518d575748a64ef9317d9c35b869b8696bbd2318
Ameera3934 <Ameerashahid@me.com>
2023-03-17 14:53:36 +0000

    Update index.js

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -55,20 +55,7 @@ export default class Iphone extends Component {
                     <button class = 'mic'>mic</button>
                     <button class ='speak'>speak</button>
 
-					<script type="text/javascript">
 
-						var myLink = document.getElementById('speak')
-						speak.onclick = function(){
-
-    						var script = document.createElement("script");
-   							script.type = "text/javascript";
-    						script.src = "chatbot.js"; 
-    						document.getElementsByTagName("head")[0].appendChild(script);
-    						return false;
-
-						}
-
-					</script>
                 </div>
 			</div>
 			// 	<div class={ style.header }>
@@ -96,4 +83,49 @@ export default class Iphone extends Component {
 			cond : conditions
 		});      
 	}
+	
+	const startBtn = document.querySelector("#speak");
+
+
+	const recognition = new webkitSpeechRecognition();
+	recognition.continuous = true;
+	recognition.lang = "en-US";
+	recognition.interimResults = false;
+	recognition.maxAlternatives = 1;
+
+	const synth = window.speechSynthesis;
+
+
+	startBtn.addEventListener("click", () =>{
+		recognition.start();
+		utter.text = "Hello. i am your chatbot for today.... some basic questions you can ask are: what is the weather like today?";
+		synth.speak(utter);
+		
+	});
+
+	let utter = new SpeechSynthesisUtterance("Hi");
+	utter.onend = () => {
+		recognition.start();
+	};
+
+	recognition.onresult = (e) =>{
+		const transcript = e.results[e.results.length -1][0].transcript.trim();
+		if (transcript === "hello"){
+			recognition.stop();
+			utter.text = "Hello";
+			synth.speak(utter);
+		}
+
+		else if (transcript ==="what is the weather like today"){
+			recognition.stop();
+			utter.text = "the weather today is relatively sunny - highest of 20 degrees";
+			synth.speak(utter);
+		};
+		else if (transcript ==="goodbye"){
+			recognition.stop();
+			utter.text = "goodbye. see you soon";
+			synth.speak(utter);
+		};
+	};
+
 }
