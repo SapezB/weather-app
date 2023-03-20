b1fd6ca342d3c028f4bba5458517c28b8f4de387
Ameera3934 <Ameerashahid@me.com>
2023-03-20 13:04:31 +0000

    Update index.js

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -40,7 +40,7 @@ export default class Iphone extends Component {
 		// check if temperature data is fetched, if so add the sign styling to the page
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
 
-		document.getElementById("speak").onclick =function(){speaking()};
+		
 		// display all weather data
 		return (
 			<div class={ style.container }>
@@ -86,53 +86,7 @@ export default class Iphone extends Component {
 		});      
 	}
 
-	speaking=()=>{
-		const recognition = new webkitSpeechRecognition();
-		recognition.continuous = true;
-		recognition.lang = "en-US";
-		recognition.interimResults = false;
-		recognition.maxAlternatives = 1;
-
-		const synth = window.speechSynthesis;
-
-		speak.addEventListener("click", () =>{
-			recognition.start();
-			utter.text = "Hello. i am your chatbot for today.... some basic questions you can ask are: what is the weather like today?";
-			synth.speak(utter);	
-		});
-
-		let utter = new SpeechSynthesisUtterance("Hi");
-		utter.onend = () => {
-			recognition.start();
-		};
-
-		recognition.onresult = (e) =>{
-			const transcript = e.results[e.results.length -1][0].transcript.trim();
-			if (transcript === "hello"){
-				recognition.stop();
-				utter.text = "Hello";
-				synth.speak(utter);
-			}
-
-			else if (transcript ==="what is the weather like today"){
-				recognition.stop();
-				utter.text = "the weather today is relatively sunny - highest of 20 degrees";
-				synth.speak(utter);
-			}
-
-			else if (transcript ==="goodbye"){
-				recognition.stop();
-				utter.text = "goodbye. see you soon";
-				synth.speak(utter);
-			}
-
-			else {
-				recognition.stop();
-				utter.text = "I dont know that command yet";
-				synth.speak(utter);
-			}
-		};
-	}
+	
 
 
 }
