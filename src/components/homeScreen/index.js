2df7a0a51457ddfaebf549fe3b033ef1828a5dc8
Saloni <salonisalian93@gmail.com>
2023-03-20 18:12:52 +0000

    Update index.js

--- src/components/homeScreen/index.js
+++ src/components/homeScreen/index.js
@@ -4,7 +4,7 @@ import Button from '../button';
 import style from './style';
 import LocScreen from '../locScreen';
 import DateScreen from '../dateScreen';
-//import Text from '../text-to-speech';
+import Text from '../text-to-speech';
 
 export default class homeScreen extends Component {
 	constructor(props) {
@@ -29,16 +29,15 @@ export default class homeScreen extends Component {
 	// switchToTalk = () => {
 	// 	this.setState({ screen: 'Text' });
 	// };
+	// App = () => {
+	//     const [ ourText, setOurText ] = useState('');
+	// 	const msg = new SpeechSynthesisUtterance();
 
-	App = () => {
-		const [ ourText, setOurText ] = useState('');
-		const msg = new SpeechSynthesisUtterance();
-
-		const speechHandler = (msg) => {
-			msg.text = ourText;
-			window.speechSynthesis.speak(msg);
-		};
-	};
+	// 	const speechHandler = (msg) => {
+	// 		msg.text = ourText;
+	// 		window.speechSynthesis.speak(msg);
+	// 	};
+	// };
 
 	render() {
 		if (this.state.screen == 'Home') {
@@ -64,7 +63,7 @@ export default class homeScreen extends Component {
 							type="text"
 							value={ourText}
 							placeholder="Enter Text"
-							onChange={(e) => setOurText(e.target.value)}
+							onInput={(e) => setOurText(e.target.value)}
 						/>
 						<button class="mic" onClick={() => speechHandler(msg)}>
 							mic
