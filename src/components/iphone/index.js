48c8ed840ec39b9a36eb294e9d2be69a7485a8c9
Ameera3934 <Ameerashahid@me.com>
2023-03-20 15:41:04 +0000

    Update index.js

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -54,7 +54,7 @@ export default class Iphone extends Component {
                 </div>
                 <div class="bot">
                     <button class = 'mic'>mic</button>
-                    <button class ='speak' >speak</button>
+                    <Button class ='speak' clickFunctio={speaking}>speak</button>
 
                 </div>
 			</div>
@@ -85,7 +85,55 @@ export default class Iphone extends Component {
 			cond : conditions
 		});      
 	}
+	speaking=()=>{
+        const recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.lang = "en-US";
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
 
+        const synth = window.speechSynthesis;
+
+
+
+        speak.addEventListener("click", () =>{
+            recognition.start();
+            utter.text = "Hello. i am your chatbot for today.... some basic questions you can ask are: what is the weather like today?";
+            synth.speak(utter); 
+        });
+
+        let utter = new SpeechSynthesisUtterance("Hi");
+        utter.onend = () => {
+            recognition.start();
+        };
+
+        recognition.onresult = (e) =>{
+            const transcript = e.results[e.results.length -1][0].transcript.trim();
+            if (transcript === "hello"){
+                recognition.stop();
+                utter.text = "Hello";
+                synth.speak(utter);
+            }
+
+            else if (transcript ==="what is the weather like today"){
+                recognition.stop();
+                utter.text = "the weather today is relatively sunny - highest of 20 degrees";
+                synth.speak(utter);
+            }
+
+            else if (transcript ==="goodbye"){
+                recognition.stop();
+                utter.text = "goodbye. see you soon";
+                synth.speak(utter);
+            }
+
+            else {
+                recognition.stop();
+                utter.text = "I dont know that command yet";
+                synth.speak(utter);
+            }
+        };
+    }
 	
 
 
