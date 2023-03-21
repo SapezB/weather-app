bc4794e93da062be702b3d05eab62508d2cce097
Ameera3934 <Ameerashahid@me.com>
2023-03-21 18:10:17 +0000

    Update index.js

--- src/components/homeScreen/index.js
+++ src/components/homeScreen/index.js
@@ -38,7 +38,49 @@ export default class homeScreen extends Component {
 	// 		window.speechSynthesis.speak(msg);
 	// 	};
 	// };
+	speaking = () =>{
+		const startBtn = document.querySelector("#speechBtn");
+		const recognition = new webkitSpeechRecognition();
+		recognition.continuous = true;
+		recognition.lang = "en-US";
+		recognition.interimResults = false;
+		recognition.maxAlternatives = 1;
 
+		const synth = window.speechSynthesis;
+
+		startBtn.addEventListener("click", () =>{
+			recognition.start();
+			utter.text = "Hello. i am your chatbot for today.... some basic questions you can ask are: what is the weather like today?";
+			synth.speak(utter);
+									
+		});
+
+		let utter = new SpeechSynthesisUtterance("Hi");
+		utter.onend = () => {
+			recognition.start();
+			};
+
+		recognition.onresult = (e) =>{
+			const transcript = e.results[e.results.length -1][0].transcript.trim();
+			if (transcript === "hello"){
+				recognition.stop();
+				utter.text = "Hello";
+				synth.speak(utter);
+			}
+
+			else if (transcript ==="what is the weather like today"){
+				recognition.stop();
+				utter.text = "the weather today is relatively sunny - highest of 20 degrees";
+				synth.speak(utter);
+			}
+			else if (transcript ==="goodbye"){
+				recognition.stop();
+				utter.text = "goodbye. see you soon";
+				synth.speak(utter);
+			}
+		};
+	}
+	
 	render() {
 		if (this.state.screen == 'Home') {
 			return (
@@ -72,6 +114,11 @@ export default class homeScreen extends Component {
 						{/*<button class="speak">speak</button>*/}
 						<Speaker />
 					</div>
+					<div class="speechA">
+						<button class="speechBtn" onClick={this.speaking}>speech-speech</button>
+							
+					</div>
+
 				</div>
 			);
 		} else if (this.state.screen == 'Set') {
