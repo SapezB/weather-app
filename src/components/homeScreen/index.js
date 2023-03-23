0095a2db0a9e34b9fa387d9b40f5bd57178b49fe
Saloni <salonisalian93@gmail.com>
2023-03-23 17:28:10 +0000

    Comments added, removed extra text-to-speech folder and code cleaned up

--- src/components/homeScreen/index.js
+++ src/components/homeScreen/index.js
@@ -3,7 +3,6 @@ import SettingsScreen from '../settingsScreen';
 import Button from '../button';
 import style from './style';
 import LocScreen from '../locScreen';
-//import Speaker from '../text-to-speech';
 import DateScreen from '../dateScreen';
 import style_iphone from '../button/style_iphone';
 import $, { data } from 'jquery';
@@ -12,55 +11,46 @@ import { FaMicrophone } from 'react-icons/fa';
 import { GiSpeaker } from 'react-icons/gi';
 import { IoMdSettings } from 'react-icons/io';
 
-function speaking123 () {
-	console.log("why");
+function speaking123() {
+	console.log('why');
 
 	const recognition = new webkitSpeechRecognition();
 	recognition.continuous = true;
-	recognition.lang = "en-US";
+	recognition.lang = 'en-US';
 	recognition.interimResults = false;
 	recognition.maxAlternatives = 1;
 
-	const weatherTodayBot = new SpeechSynthesisUtterance("The weather today is very sunny with a highest of 20 degrees.");
-	const goodbyeBot = new SpeechSynthesisUtterance("Goodbye");
-	const hi = new SpeechSynthesisUtterance("hello");
-	const tomorrowWeatherBot = new SpeechSynthesisUtterance("it will be quite warm tomorrow with a high of 25 degrees");
-	const rainTomorrowBot= new SpeechSynthesisUtterance("not that i know of");
-	const another= new SpeechSynthesisUtterance("please press the button again to ask another question");
+	const weatherTodayBot = new SpeechSynthesisUtterance(
+		'The weather today is very sunny with a highest of 20 degrees.'
+	);
+	const goodbyeBot = new SpeechSynthesisUtterance('Goodbye');
+	const hi = new SpeechSynthesisUtterance('hello');
+	const tomorrowWeatherBot = new SpeechSynthesisUtterance('it will be quite warm tomorrow with a high of 25 degrees');
+	const rainTomorrowBot = new SpeechSynthesisUtterance('not that i know of');
+	const another = new SpeechSynthesisUtterance('please press the button again to ask another question');
 
-
-	recognition.onresult = function(event){
+	recognition.onresult = function(event) {
 		const speechToTex = event.results[0][0].transcript;
 		console.log(speechToTex);
-		if (speechToTex.toLowerCase().includes("what is the weather like today")) {
+		if (speechToTex.toLowerCase().includes('what is the weather like today')) {
 			window.speechSynthesis.speak(weatherTodayBot);
 			window.speechSynthesis.speak(another);
-		} 
-		else if (speechToTex.toLowerCase().includes("goodbye")) {
+		} else if (speechToTex.toLowerCase().includes('goodbye')) {
 			window.speechSynthesis.speak(goodbyeBot);
 			recognition.stop();
-
-		} 
-		else if (speechToTex.toLowerCase().includes("will it rain today")) {
-			window.speechSynthesis.speak(rainTomorrowBot);	
+		} else if (speechToTex.toLowerCase().includes('will it rain today')) {
+			window.speechSynthesis.speak(rainTomorrowBot);
 			window.speechSynthesis.speak(another);
-
-		} 
-		else if (speechToTex.toLowerCase().includes("what is the weather like tomorrow")) {
+		} else if (speechToTex.toLowerCase().includes('what is the weather like tomorrow')) {
 			window.speechSynthesis.speak(tomorrowWeatherBot);
 			window.speechSynthesis.speak(another);
-
-		} 
-		else if (speechToTex.toLowerCase().includes("hello")) {
+		} else if (speechToTex.toLowerCase().includes('hello')) {
 			window.speechSynthesis.speak(hi);
 			window.speechSynthesis.speak(another);
 		}
-
 	};
 	recognition.start();
-
-
-};
+}
 
 export default class homeScreen extends Component {
 	constructor(props) {
@@ -73,21 +63,21 @@ export default class homeScreen extends Component {
 			category: this.props.category,
 			display: true,
 			currentDate: date,
-            location: 'London' ,
-            unit:'metric'
+			location: 'London',
+			unit: 'metric'
 		};
-		this.msg = new SpeechSynthesisUtterance();
+		this.msg = new SpeechSynthesisUtterance(); //creates an object to turn text into speech
 	}
 
-    handleLocationChange = (e) => {
-        this.setState({location: e.target.value});
-        this.props.setLocation(e.target.value);
-      }
-      
-    handleUnitChange = (e) => {
-        this.setState({unit: e.target.value});
-        this.props.setUnit(e.target.value);
-    }
+	handleLocationChange = (e) => {
+		this.setState({ location: e.target.value });
+		this.props.setLocation(e.target.value);
+	};
+
+	handleUnitChange = (e) => {
+		this.setState({ unit: e.target.value });
+		this.props.setUnit(e.target.value);
+	};
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
@@ -106,59 +96,69 @@ export default class homeScreen extends Component {
 		});
 		// once the data grabbed, hide the button
 		this.setState({ display: false });
-	}
+	};
 
-	switchToSet = () => { //function to switch to settingscreen
+	switchToSet = () => {
+		//function to switch to settingscreen
 		this.setState({ screen: 'Set' });
 	};
-    handleLocationChange = (e) => { //function to change location
-        this.setState({location: e.target.value});
-        this.props.setLocation(e.target.value);
-      }
-      
-    handleUnitChange = (e) => { //changes the unit
-        this.setState({unit: e.target.value});
-        this.props.setUnit(e.target.value);
-    }
+	handleLocationChange = (e) => {
+		//function to change location
+		this.setState({ location: e.target.value });
+		this.props.setLocation(e.target.value);
+	};
 
-    	// a call to fetch weather data via wunderground
+	handleUnitChange = (e) => {
+		//changes the unit
+		this.setState({ unit: e.target.value });
+		this.props.setUnit(e.target.value);
+	};
+
+	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
-        const location = this.props.location || 'London';
-        const unit = this.props.unit|| 'metric';
+		const location = this.props.location || 'London';
+		const unit = this.props.unit || 'metric';
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
 		// my API key: 1812aa1047a527c9537d5e2315c80ba0
 		// another API key in case the other one gets blocked: 2915cce6a56b729abba54554662da808
-		var url = "http://api.openweathermap.org/data/2.5/weather?q="+location+"&units="+unit+"&APPID=2915cce6a56b729abba54554662da808";
+		var url =
+			'http://api.openweathermap.org/data/2.5/weather?q=' +
+			location +
+			'&units=' +
+			unit +
+			'&APPID=2915cce6a56b729abba54554662da808';
 		$.ajax({
 			url: url,
-			dataType: "jsonp",
-			success : this.parseResponse,
-			error : function(req, err){ console.log('API call failed ' + err); }
-		})
+			dataType: 'jsonp',
+			success: this.parseResponse,
+			error: function(req, err) {
+				console.log('API call failed ' + err);
+			}
+		});
 		// once the data grabbed, hide the button
 		this.setState({ display: false });
+	};
+
+	printLoc() {
+		//changes to the lock screen
+
+		console.log('Loc: ', this.state.location);
 	}
 
-   
-    printLoc() {//changes to the lock screen
-
-        console.log("Loc: ",this.state.location);
-    }
-
-
-
-	switchToDate = () => { //changes to the screen for 24 hour forcast
+	switchToDate = () => {
+		//changes to the screen for 24 hour forcast
 		this.setState({ screen: 'Date' });
 	};
 
-	speechHandler = () => { //reads the temperature out loud
-		const message = 'The temperature is'.concat(this.state.temp);
+	speechHandler = () => {
+		//reads the temperature out loud
+		const message = 'The temperature is'.concat(this.state.temp); //takes the temperature from the api and concatonates it to a String
 		this.msg.text = message;
 		window.speechSynthesis.speak(this.msg);
 	};
 
 	render() {
-        //homnescreen
+		//homnescreen
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
 		if (this.state.screen == 'Home') {
 			return (
@@ -172,7 +172,7 @@ export default class homeScreen extends Component {
 							<IoMdSettings />
 						</button>
 					</div>
-						{/* <div class="mid"> */}
+					{/* <div class="mid"> */}
 					<div class={style.header}>
 						<div class={style.city}>{this.state.locate}</div>
 						{/* <div><img src="http://openweathermap.org/img/wn/${
@@ -192,30 +192,14 @@ export default class homeScreen extends Component {
 						) : null}
 					</div>
 					<div class={style_iphone.container}>
-							<button class="style_iphone.button" onClick={speaking123}>
-								<FaMicrophone />
-							</button>
-							<button class="style_iphone.button" onClick={this.speechHandler}>
-								<GiSpeaker />
-							</button>
+						<button class="style_iphone.button" onClick={speaking123}>
+							<FaMicrophone />
+						</button>
+						<button class="style_iphone.button" onClick={this.speechHandler}>
+							<GiSpeaker />
+						</button>
 					</div>
-							{/* <div class="bot"> */}
-								{/*<input*/}
-								{/*	class="try"*/}
-								{/*	type="text"*/}
-								{/*	value={ourText}*/}
-								{/*	placeholder="Enter Text"*/}
-								{/*	onInput={(e) => setOurText(e.target.value)}*/}
-								{/*/>*/}
-								{/* <button class="mic" onClick={this.speechHandler}>
-									mic
-								</button> */}
-								{/*/!* <button class="mic">mic</button> *!/*/}
-								{/*<button class="speak">speak</button>*/}
-								{/* <Speaker /> */}
-							{/* </div> */}
 				</div>
-				// </div>
 			);
 		} else if (this.state.screen == 'Set') {
 			return (
